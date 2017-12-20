9a91b557bbd518c0dcef3cb2fec4edfb3d095ede
ej <evanrjohnso@gmail.com>
2017-12-20 12:59:27 -0800

    style about me
    restyle about for mobile viewing
    
    mobile styling
    
    add icon images
    
    style icons and add links
    
    add custom to header
    
    add custom pictures
    
    add styling for large screens
    
    quick fix
    
    fix about image overflowing parent div
    
    css refactor
    
    css refactoring
    
    add about me content
    
    cleanup styling and fix image and text overlap bug
    
    mobile styling
    
    mobile styling
    
    mobile styling
    
    update about me content
    
    quick updates
    
    add typewriter effect
    
    refactor typeWriter
    
    further refactor for readability
    
    add timeout to delay type writer functionality
    
    mobile styling updates
    
    micro styling changes
    
    add minify scripts to build pipeline
    
    fixx weird link styling issue in footer
    
    resizing bug resolved
    
    update about me copy
    
    update copy and styling about
    
    image loaded function
    
    revert to old commit to grab old styling, weird
    
    create closure around typeWriter function to encapsulate private variables
    
    refactor document click function to be separate function
    
    refactor scroll to document location to be separate function, increasing readibility
    
    change gulp build command from dev to serve
    
    change picture
    
    change picture again
    
    updage picture again

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,51 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var pkg = require('./package.json');
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var pkg = require("./package.json");
+var uglify = require("gulp-uglify");
 
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
-gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
+gulp.task("copy", function() {
+  gulp
+    .src([
+      "node_modules/bootstrap/dist/**/*",
+      "!**/npm.js",
+      "!**/bootstrap-theme.*",
+      "!**/*.map"
     ])
-    .pipe(gulp.dest('vendor/bootstrap'))
+    .pipe(gulp.dest("vendor/bootstrap"));
 
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
-})
+  gulp
+    .src([
+      "node_modules/jquery/dist/jquery.js",
+      "node_modules/jquery/dist/jquery.min.js"
+    ])
+    .pipe(gulp.dest("vendor/jquery"));
+});
 
 // Default task
-gulp.task('default', ['copy']);
+gulp.task("default", ["copy"]);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task("browserSync", function() {
   browserSync.init({
     server: {
-      baseDir: ''
-    },
-  })
-})
+      baseDir: ""
+    }
+  });
+});
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync'], function() {
+gulp.task("serve", ["browserSync", "minifyScripts"], function() {
   // Reloads the browser whenever HTML or CSS files change
-  gulp.watch('js/*.js', browserSync.reload);
-  gulp.watch('css/*.css', browserSync.reload);
-  gulp.watch('*.html', browserSync.reload);
+  gulp.watch("js/*.js", browserSync.reload);
+  gulp.watch("css/*.css", browserSync.reload);
+  gulp.watch("*.html", browserSync.reload);
+});
+
+gulp.task("minifyScripts", function() {
+  return gulp
+    .src("./build/js/scripts.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js/"));
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,46 @@
 $(document).ready( () => {
-  $('.nav-link').click( (e) => {
-    const ref = e.target.getAttribute('href');
-      $('html, body').animate({
-        scrollTop: $(ref).offset().top
-    }, 400);
-  });
-  $(document).click( event => {
-    var clickover = $(event.target);
-    var $navbar = $("#navbarResponsive");
-    var _opened = $navbar.hasClass('collapse');
-    if (_opened && !clickover.hasClass("nav-item")) {
-      $navbar.collapse('hide');
-    };
-  });
+  $(document).click(collapsingNavBar);
+  $('.nav-link').click(scrollTo);
+  $('header').imagesLoaded( {background: true}, typeHeaders);
 });
+
+function scrollTo(e) {
+  if (!e) { return; }
+  const ref = e.target.getAttribute('href');
+  $('html, body').animate({
+    scrollTop: $(ref).offset().top
+  }, 400);
+}
+function collapsingNavBar(e) {
+  console.log(e)
+  let clickover = $(e.target);
+  let $navbar = $("#navbarResponsive");
+  let _opened = $navbar.hasClass('collapse');
+  if (_opened && !clickover.hasClass("nav-item")) {
+    $navbar.collapse('hide');
+  };
+}
+function typeHeaders() {
+
+  const header1 = "Evan Johnson";
+  const header2 = "Web & Mobile Developer";
+  const speed = 150;
+  
+  let name = document.getElementById("header-name");
+  let title = document.getElementById("header-developer");
+  
+  let finished = false;
+  typeWriter(name, header1, 0);
+  function typeWriter(html, txt, charCounter) {
+    if (finished) return;
+    if (charCounter < txt.length) {
+      html.innerHTML += txt.charAt(charCounter);
+      charCounter++;
+      setTimeout(typeWriter.bind(this, html, txt, charCounter), speed);
+      return;
+    }
+    if (txt === header2) finished = true;
+    // setTimeout(typeWriter(title, header2, 0), 3000);
+    typeWriter(title, header2, 0)
+  }
+};
