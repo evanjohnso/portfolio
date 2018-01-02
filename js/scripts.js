e52b521431ab6930450bff8a0e6c1fa4163f60fa
ej <evanrjohnso@gmail.com>
2018-01-02 10:56:11 -0800

    mobile styling updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 $(document).ready( () => {
+
   $('.nav-link').click( (e) => {
     const ref = e.target.getAttribute('href');
       $('html, body').animate({
         scrollTop: $(ref).offset().top
     }, 400);
   });
-  $(document).click( event => {
-    var clickover = $(event.target);
-    var $navbar = $("#navbarResponsive");
-    var _opened = $navbar.hasClass('collapse');
+  $(document).click( e => {
+    let clickover = $(e.target);
+    let $navbar = $("#navbarResponsive");
+    let _opened = $navbar.hasClass('collapse');
     if (_opened && !clickover.hasClass("nav-item")) {
       $navbar.collapse('hide');
     };
